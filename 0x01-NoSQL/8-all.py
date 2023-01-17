af91a81cdfcba4b043724a22d25902522274e73e
eyasuyid <eyasuyid@gmail.com>
2023-01-17 13:11:09 +0300

    list all

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module 8 operate on mongo collections"""
+
+
+def list_all(mongo_collection):
+    """returns [] if null or empty
+    or return all items in list"""
+
+    return [item for item in mongo_collection]
